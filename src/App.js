bc861c33bf7a5d415e92ee8bc9f2f6b1d7cc7c11
Aadarshprajapati777 <aadarsh9811594152@gmail.com>
2025-02-28 13:35:43 +0530

    fix:route to skill page and add chatbot component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Routes } from "react-router-dom";
 import HomePage from './Pages/Homepage';
+import SkillsPage from './Pages/SkillsPage';
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/SkillsPage" element={<SkillsPage />} />
 
 
       </Routes>
