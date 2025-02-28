a5633b1dd839270f90b666bc0b3505eaa3717fb4
Aadarshprajapati777 <aadarsh9811594152@gmail.com>
2025-02-28 11:15:20 +0530

    working on main content of homepage

--- src/components/MainContent/MainContent.jsx
+++ src/components/MainContent/MainContent.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import { MainContent } from "../components/MainContent/MainContent";
 const HomePage = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
-      olabs hackathon begins
-      <MainContent />
+        main content
     </div>
   );
 };
--- src/components/index.jsx
+++
@@ -1,4 +0,0 @@
-export { default as Header } from './Header/Header';
-export { default as Navigation } from './Navigation/Navigation';
-export { default as MainContent } from './MainContent/MainContent';
-export { default as Footer } from './Footer/Footer';
