8bcf0f964e4e494ffeaca0523a73f9a3e4542e0b
Aadarshprajapati777 <aadarsh9811594152@gmail.com>
2025-02-28 11:19:17 +0530

    working on main content of the page

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import MainContent from '../components/MainContent/MainContent'
 const HomePage = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <Header />
+      <MainContent />
     </div>
   );
 };
