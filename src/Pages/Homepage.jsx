2116247c23bd79fb5755f0d0aedde834e315ecb4
Aadarshprajapati777 <aadarsh9811594152@gmail.com>
2025-02-28 11:21:15 +0530

    merged and added main content

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Navigation from "../components/Navigation/Navigation";
+import Maincontent  from "../components/MainContent/MainContent";
 const HomePage = () => {
   return (
     <div className="min-h-screen flex flex-col bg-gray-50">
       <Header />
       <Navigation />
-      
+      <Maincontent />
     </div>
   );
 };
